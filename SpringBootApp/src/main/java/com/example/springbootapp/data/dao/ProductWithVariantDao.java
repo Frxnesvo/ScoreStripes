80dfa2880c9dabac41c3f0df9839e28007a71ac1
Frxnesvo <francescogallo2002@gmail.com>
2024-05-15 17:44:23 +0200

    Aggiunto l'endpoint per il numero di varianti out of stock

--- SpringBootApp/src/main/java/com/example/springbootapp/data/dao/ProductWithVariantDao.java
+++ SpringBootApp/src/main/java/com/example/springbootapp/data/dao/ProductWithVariantDao.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductWithVariantDao extends JpaRepository<ProductWithVariant, String> {
+    Integer countAllByAvailabilityEquals(int availability);
 }
--- SpringBootApp/src/main/java/com/example/springbootapp/service/interfaces/ProductWithVariantService.java
+++ SpringBootApp/src/main/java/com/example/springbootapp/service/interfaces/ProductWithVariantService.java
@@ -1,4 +1,5 @@
 package com.example.springbootapp.service.interfaces;
 
 public interface ProductWithVariantService {
+    Integer countVariantsOutOfStock();
 }
