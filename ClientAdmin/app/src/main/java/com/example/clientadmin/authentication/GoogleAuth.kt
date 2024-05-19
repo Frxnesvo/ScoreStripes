aad34839bc469066727671f059cb2363c5b7b4a0
FrancescoCavaliere11 <francescounical@gmail.com>
2024-05-19 12:00:34 +0200

    aggiunto package authentication, con una classe per effettuare il login con google. Aggiunto googleUserDto e LoginFormViewModel (da completare)

---
+++ ClientAdmin/app/src/main/java/com/example/clientadmin/authentication/GoogleAuth.kt
@@ -0,0 +1,69 @@
+package com.example.clientadmin.authentication
+
+import android.content.Context
+import android.util.Log
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.GetCredentialException
+import com.example.clientadmin.model.DTO.GoogleUserDto
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+
+
+class GoogleAuth(private val context: Context){
+    private val credentialManager = CredentialManager.create(context)
+    private val WEB_SERVER_CLIENT_ID = "774497332630-engaqhas88dv6rpmrpjfnad09i6jloef.apps.googleusercontent.com"
+
+    private fun buildGoogleIdOption(): GetGoogleIdOption {
+        return GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false) // Query all google accounts on the device
+            .setServerClientId(WEB_SERVER_CLIENT_ID)
+            .build()
+    }
+
+    private fun buildRequest(googleIdOption: GetGoogleIdOption): GetCredentialRequest {
+        return GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+    }
+
+    suspend fun getGoogleCredential() : GoogleUserDto?{
+        val googleIdOption = buildGoogleIdOption()
+        val request = buildRequest(googleIdOption)
+
+        try {
+            val result = credentialManager.getCredential(context, request)
+            return handleSignIn(result)
+
+        } catch (e: GetCredentialException) {
+            Log.e("Authentication", "GetCredentialException", e)
+        }
+
+        return null
+    }
+
+    private fun handleSignIn(result: GetCredentialResponse) : GoogleUserDto? {
+        //Implementazione per gestire il risultato del login
+        val credential = result.credential
+
+        if(credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL){
+            try {
+                val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+                val idToken = googleIdTokenCredential.idToken
+                //TODO: salvare l'id token con accountManager
+
+                val firstName = googleIdTokenCredential.givenName
+                val lastName = googleIdTokenCredential.familyName
+                val email = googleIdTokenCredential.id
+
+                return GoogleUserDto(firstName, lastName, email)
+            }catch (e: GoogleIdTokenParsingException) {
+                Log.e("INVALID GOOGLE ID TOKEN", "Received an invalid google id token response", e)
+            }
+        }
+
+        return null
+    }
+}
