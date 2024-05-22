b3603645c1154db043aab4fc9d2f044755085e8a
Frxnesvo <francescogallo2002@gmail.com>
2024-05-23 01:21:22 +0200

    Implementata la logica per reperire chi ha creato le entità (audit logging)

--- SpringBootApp/src/main/java/com/example/springbootapp/logging/AuditorAwareImpl.java
+++ SpringBootApp/src/main/java/com/example/springbootapp/logging/AuditorAwareImpl.java
@@ -1,14 +1,24 @@
 package com.example.springbootapp.logging;
 
+import com.example.springbootapp.service.impl.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component("auditorProvider")
+@RequiredArgsConstructor
 public class AuditorAwareImpl implements AuditorAware<String> {
+
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        //LOGICA PER OTTENERE L'USERNAME DELL'UTENTE LOGGATO E RESTITUIRLO COME OPTIONAL
-        return Optional.of("admin");
+        try {
+            return Optional.of(userDetailsService.getCurrentUser().getUsername());
+        } catch (Exception e) {
+            return Optional.of("system");
+        }
     }
 }
