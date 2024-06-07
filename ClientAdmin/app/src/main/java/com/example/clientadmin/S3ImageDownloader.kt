f137f2b756e9c3f37d5bc0b6553190fab8a1cb8b
FrancescoCavaliere11 <francescounical@gmail.com>
2024-06-07 19:48:45 +0200

    aggiunto downloader delle immagini dal bucket aws in client admin

---
+++ ClientAdmin/app/src/main/java/com/example/clientadmin/S3ImageDownloader.kt
@@ -0,0 +1,35 @@
+package com.example.clientadmin
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+class S3ImageDownloader {
+    companion object {
+        fun getImageFromPresignedUrl(presignedUrl: String): Flow<Bitmap> = flow {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url(presignedUrl)
+                    .build()
+
+                client.newCall(request).execute().use { response ->
+                    if (!response.isSuccessful) throw IOException("Error getting image from bucket: $response")
+
+                    response.body?.let { body ->
+                        val image = body.bytes()
+                        emit(BitmapFactory.decodeByteArray(image, 0, image.size))
+                    }
+                }
+            }
+            catch (e : Exception){
+                println("Exception getting image from bucket: ${e.message}")
+            }
+        }
+    }
+}
