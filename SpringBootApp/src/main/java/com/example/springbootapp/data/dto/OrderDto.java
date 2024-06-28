082b282c93759eec635a17b9d1f7ae00c2c435b7
Frxnesvo <francescogallo2002@gmail.com>
2024-06-28 10:47:08 +0200

    Fix mapping da order a OrderDto

--- SpringBootApp/src/main/java/com/example/springbootapp/data/dto/OrderDto.java
+++ SpringBootApp/src/main/java/com/example/springbootapp/data/dto/OrderDto.java
@@ -5,7 +5,7 @@ import com.example.springbootapp.data.entities.Enums.OrderStatus;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -13,7 +13,7 @@ import java.util.List;
 public class OrderDto {
     private String id;
     private Double totalPrice;
-    private LocalDate date;
+    private LocalDateTime date;
     private OrderStatus status;
     private OrderInformations resilientInfos;
     private List<OrderItemDto> items;
