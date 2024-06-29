61e07e0acb09b37c137e44568c6f6c475022f048
Frxnesvo <francescogallo2002@gmail.com>
2024-06-29 16:46:53 +0200

    minor fix

--- SpringBootApp/src/main/java/com/example/springbootapp/data/dao/WishlistAccessDao.java
+++ SpringBootApp/src/main/java/com/example/springbootapp/data/dao/WishlistAccessDao.java
@@ -2,6 +2,8 @@ package com.example.springbootapp.data.dao;
 
 import com.example.springbootapp.data.entities.WishlistAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.Optional;
 
 @Repository
 public interface WishlistAccessDao extends JpaRepository<WishlistAccess, String> {
+    @Modifying
+    @Query("DELETE FROM WishlistAccess wa WHERE wa.wishlist.id = :wishlistId")
     void deleteByWishlistId(String wishlistId);
 
     List<WishlistAccess> findAllByGuestId(String id);
